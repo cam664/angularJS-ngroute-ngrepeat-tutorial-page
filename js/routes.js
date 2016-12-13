cf2786e7cf61d8bae7765265a0124135525b14cd
Cameron Laing <c.laing@live.com>
2016-12-13 00:40:21 -0500

    remove unnecessary comment

--- js/routes.js
+++ js/routes.js
@@ -1,20 +1,3 @@
-/*
-app.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-    when('#fries_01', {//if someone goes to /page/1
-      templateUrl: 'partials/fries_01.html',//use the page_01.html and load it into ngView
-      controller: 'controllerPage01'//use the controllerPage01 controller
-    }).
-    when('/page/2', {//if someone goes to /page/2
-      templateUrl: 'partials/page_02.html',//use the page_02.html and load it into ngView
-      controller: 'controllerPage02'//use the controllerPage02 controller
-    }).
-	otherwise({
-      redirectTo: '/page/1'//If someone goes to any other page, redirect them to /page/1
-  });
-}]);
-*/
 //Add a config 
 app.config(['$routeProvider',
   function($routeProvider) {
